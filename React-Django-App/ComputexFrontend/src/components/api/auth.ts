c8a3679699de5a0d85f3c54e2fcd3ca5f320bec2
Laura <lauraamanda56@gmail.com>
2025-04-14 05:26:02 +0100

    test_1

--- React-Django-App/ComputexFrontend/src/components/api/auth.ts
+++ React-Django-App/ComputexFrontend/src/components/api/auth.ts
@@ -4,7 +4,7 @@ import axios from "axios";
 // const BASE_URL = "http://localhost:8000";
 // const BASE_URL = process.env.VITE_API_URL || "http://13.48.123.217:8000";
 // const BASE_URL = process.env.VITE_API_URL || "http://13.48.123.217:8000";
- const BASE_URL = "http://52.206.74.69:8000";
+ const BASE_URL = "http://52.55.60.18:8000";
 
 export const login = async (username: string, password: string) => {
   try {
